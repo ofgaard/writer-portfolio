104017eaa0f55fd6ceff54e2b9ee6f35d8cde937
Oliver <oliverfruergaard@gmail.com>
2025-12-12 15:21:11 +0100

    add layout for story headers to avoid overflowing

--- app/(frontend)/stories/[id]/page.tsx
+++ app/(frontend)/stories/[id]/page.tsx
@@ -18,18 +18,20 @@ export default async function StoryPage({ params }: { params: Promise<{ id: stri
   return (
     <div className="flex flex-col gap-6">
       <div className="flex flex-col md:flex-row gap-6 border-b md:h-screen">
-        <div className="flex flex-col gap-3 items-center text-center justify-center p-10 w-full md:w-1/2">
-          <div className="text-sm uppercase font-bold">
-            <p>{story.tag}</p>
-          </div>
-          <div className="text-xl md:hidden">
-            <p>{story.custom_tag && <span className="font-extrabold">{story.custom_tag}.</span>} {story.subheader}</p>
-          </div>
-          <h1 className="text-7xl mt-5 md:mt-0">{story.title}</h1>
-          <div className="text-2xl mt-10 hidden md:block">
-            <p>{story.custom_tag && <span className="font-extrabold">{story.custom_tag}.</span>} {story.subheader}</p>
-          </div>
-        </div>
+   <div className="flex flex-col gap-3 items-center text-center justify-center p-4 md:p-10 w-full md:w-1/2">
+  <div className="text-sm uppercase font-bold">
+    <p>{story.tag}</p>
+  </div>
+  <div className="text-xl md:hidden">
+    <p>{story.custom_tag && <span className="font-extrabold">{story.custom_tag}.</span>} {story.subheader}</p>
+  </div>
+  <h1 className="text-4xl md:text-5xl lg:text-7xl mt-5 md:mt-0 w-full px-4">
+    {story.title}
+  </h1>
+  <div className="text-2xl mt-10 hidden md:block">
+    <p>{story.custom_tag && <span className="font-extrabold">{story.custom_tag}.</span>} {story.subheader}</p>
+  </div>
+</div>
           {story.image && (
           <div className="relative w-full hidden md:block md:w-1/2 h-full">
             <Image 
